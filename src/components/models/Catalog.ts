284789488e4375cb781ed67d5f10f4e5ebce1526
mrnobody <corvus007@yandex.ru>
2025-11-01 16:40:56 +0300

    Реализует класс `Catalog`

---
+++ src/components/models/Catalog.ts
@@ -0,0 +1,26 @@
+import {IProduct} from "../../types";
+
+export class Catalog {
+    private products: IProduct[] = [];
+    private currentItem: IProduct | null = null;
+
+    setItems(products: IProduct[]) {
+        this.products = products;
+    }
+
+    getItems(): IProduct[] {
+        return this.products;
+    }
+
+    getItem(productId: string): IProduct | null {
+        return this.products.find(({id}) => id === productId) ?? null;
+    }
+
+    setCurrentItem(product: IProduct | null): void {
+        this.currentItem = product;
+    }
+
+    getCurrentItem(): IProduct | null {
+        return this.currentItem;
+    }
+}
